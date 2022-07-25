c694102fdcf7094cfba168671f653b5293993cf6
roman02021 <roman.zemanik@revolware.com>
2022-07-25 22:38:25 +0200

    aa

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -2,7 +2,7 @@
 import { ref} from 'vue'
 import useMediaQuery from '../useMediaQuery';
 import Input from './Input.vue';
-import CheckBox from './CheckBox.vue';
+import CheckBox from '@/components/CheckBox.vue';
 import Button from './Button.vue';
 import Icon from './icons/Icon.vue';
 import {themeStore} from '../store';
