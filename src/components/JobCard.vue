1fc16c65ba1d0be8489081f497ffce3fcb97dfb6
roman02021 <roman.zemanik@revolware.com>
2022-07-26 08:11:59 +0200

    a

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -50,8 +50,8 @@ const logoBackground = computed(() => {
 
   <div class="job-card" :class="{'job-card--dark': themeStore.isDark()}">
   <slot/>
-    <div class="job-card__logo-background" :style="newUrl">
-      <img class="job-card__logo"  :src="logo"/>
+    <div class="job-card__logo-background" :style="logoBackground">
+      <img class="job-card__logo"  :src="newUrl"/>
     </div>
     <div class="job-card__info-container">
       <span class="job-card__info">
